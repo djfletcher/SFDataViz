a885b23f4912a3e3967628f0755b42cbcd68781a
Daniel <daniel.j.fletcher2@gmail.com>
2017-05-25 10:43:37 -0700

    Write configure store and root reducer

--- frontend/entry.jsx
+++ frontend/entry.jsx
@@ -1,9 +1,6 @@
 import ReactDOM from 'react-dom';
 import React from 'react';
 import App from './components/app';
-import { fetchCrimes } from './util/crimes_api_util';
-
-window.fetchCrimes = fetchCrimes;
 
 window.addEventListener("DOMContentLoaded", () => {
   const root = document.getElementById("root");
---
+++ frontend/reducers/root_reducer.js
@@ -0,0 +1,8 @@
+import { combineReducers } from 'redux';
+import CrimeReducer from 'crime_reducer';
+
+const RootReducer = combineReducers({
+  crime: CrimeReducer
+});
+
+export default RootReducer;
