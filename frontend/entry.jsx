a655f6ca851a548a34eeb278708a5f3dc33a019c
Daniel <daniel.j.fletcher2@gmail.com>
2017-05-25 10:28:19 -0700

    Write crime API util working

--- frontend/entry.jsx
+++ frontend/entry.jsx
@@ -1,6 +1,9 @@
 import ReactDOM from 'react-dom';
 import React from 'react';
 import App from './components/app';
+import { fetchCrimes } from './util/crimes_api_util';
+
+window.fetchCrimes = fetchCrimes;
 
 window.addEventListener("DOMContentLoaded", () => {
   const root = document.getElementById("root");
---
+++ frontend/util/crimes_api_util.js
@@ -0,0 +1,34 @@
+export const fetchCrimes = offset => (
+  $.ajax({
+    url: 'https://data.sfgov.org/resource/9v2m-8wqu.json',
+    data: {
+      '$select': 'location,date,category',
+      '$where': params.join(` OR `),
+      '$offset': offset
+    },
+    headers: {
+      'X-App-Token': appToken
+    }
+  })
+);
+
+// Crimes we care about -- API requires them to be inside single quotes
+const violentCrime = [
+  "'ARSON'",
+  "'ASSAULT'",
+  "'BURGLARY'",
+  "'DRIVING UNDER THE INFLUENCE'",
+  "'DRUG/NARCOTIC'",
+  "'KIDNAPPING'",
+  "'LARCENY/THEFT'",
+  "'PROSTITUTION'",
+  "'ROBBERY'",
+  "'SEX OFFENSES, FORCIBLE'",
+  "'SEX OFFENSES, NON FORCIBLE'",
+  "'STOLEN PROPERTY'",
+  "'VEHICLE THEFT'",
+  "'WEAPON LAWS'"
+];
+
+const params = violentCrime.map(category => `category=${category}`);
+const appToken = 'Eb5er7pn8pszkiDz2g9g7oQmp';
